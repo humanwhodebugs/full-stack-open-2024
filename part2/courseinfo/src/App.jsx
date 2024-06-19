0aada1b03c586190d0eee2bce703a2ea875d0aa4
Muhammad Rifai <95411106+zeitakubyoo@users.noreply.github.com>
2024-06-19 16:36:44 +0700

    2.5: Separate module step 10

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,3 +1,5 @@
+import Course from "./components/Course";
+
 const App = () => {
   const courses = [
     {
@@ -45,53 +47,6 @@ const App = () => {
   ];
 
   return <Course courses={courses} />;
-};
-
-const Course = ({ courses }) => {
-  return (
-    <div>
-      <Header />
-      {courses.map((course) => (
-        <Content key={course.id} name={course.name} parts={course.parts} />
-      ))}
-    </div>
-  );
-};
-
-const Header = () => {
-  return <h1>Web Development Curriculum</h1>;
-};
-
-const Content = ({ name, parts }) => {
-  const total = parts.reduce(
-    (accumulator, part) => accumulator + part.exercises,
-    0
-  );
-  return (
-    <div>
-      <h2>{name}</h2>
-      {parts.map((part) => (
-        <Part key={part.id} exercises={part.exercises} text={part.name} />
-      ))}
-      <Total total={total} />
-    </div>
-  );
-};
-
-const Part = ({ exercises, text }) => {
-  return (
-    <p>
-      {text} {exercises}
-    </p>
-  );
-};
-
-const Total = ({ total }) => {
-  return (
-    <p>
-      <b>Total of {total} exercises</b>
-    </p>
-  );
 };
 
 export default App;
