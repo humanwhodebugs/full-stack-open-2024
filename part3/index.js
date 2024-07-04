cb839d070a59595d97a593637bdc17f5eb04e6d8
Muhammad Rifai <95411106+zeitakubyoo@users.noreply.github.com>
2024-07-04 18:46:19 +0700

    3.18*: Phonebook database step 6

--- part3/index.js
+++ part3/index.js
@@ -10,34 +10,7 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static("dist"));
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 const getMorgan = morgan("tiny");
-
-app.get("/", getMorgan, (request, response) => {
-  response.send("<h1>Phonebook Backend</h1>");
-});
 
 app.get("/api/persons", getMorgan, (request, response, next) => {
   Person.find({})
@@ -47,15 +20,10 @@ app.get("/api/persons", getMorgan, (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", getMorgan, (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.get("/api/persons/:id", getMorgan, (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((result) => response.json(result))
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response, next) => {
@@ -64,13 +32,18 @@ app.delete("/api/persons/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.get("/info", getMorgan, (request, response) => {
+app.get("/info", (request, response, next) => {
   const date = new Date();
-  const personCount = persons.length;
-  response.send(`
-    <p>Phonebook has info for ${personCount} people.</p>
-    <p>${date}</p>
-  `);
+  Person.find({})
+    .then((result) => {
+      response.send(
+        `
+        <p>Phonebook has info for ${result.length} persons</p>
+        <p>${date}</p>
+        `
+      );
+    })
+    .catch((error) => next(error));
 });
 
 const postMorgan = morgan(
