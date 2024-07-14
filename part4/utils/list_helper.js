e99cca5ccb5f6ccf2995c051337c84b9995a1b4d
Muhammad Rifai <95411106+zeitakubyoo@users.noreply.github.com>
2024-07-14 21:06:51 +0700

    4.6*: Helper Functions and Unit Tests, step 4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 const dummy = (blogs) => {
   return 1;
 };
@@ -22,8 +24,23 @@ const favoriteBlog = (blogs) => {
   };
 };
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  const authors = _.countBy(blogs, 'author');
+  const topAuthor = _.maxBy(_.keys(authors), (author) => authors[author]);
+
+  return {
+    author: topAuthor,
+    blogs: authors[topAuthor],
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };
