9d3b43a8516a68175b2a21ad89e356dc7a3f9eb6
Muhammad Rifai <muhmmd.rifaii@gmail.com>
2024-08-12 12:43:02 +0700

    5.7 Blog List Frontend, step 7

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>{visible ? 'Hide' : 'View'}</button>
+      </div>
+      {visible && (
+        <div>
+          <p>{blog.url}</p>
+          <p>
+            Likes {blog.likes}
+            <button>like</button>
+          </p>
+          <p>{blog.user.name}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
