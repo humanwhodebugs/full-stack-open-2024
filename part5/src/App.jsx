df562eb2602dae9d114772cb676950e10f7b1d1e
Muhammad Rifai <muhmmd.rifaii@gmail.com>
2024-08-12 13:51:43 +0700

    5.10: Blog List Frontend, step 10

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -19,7 +19,10 @@ const App = () => {
   const newBlogRef = useRef();
 
   useEffect(() => {
-    blogService.getAll().then((blogs) => setBlogs(blogs));
+    blogService.getAll().then((blogs) => {
+      const sortedBlogs = blogs.sort((a, b) => b.likes - a.likes);
+      setBlogs(sortedBlogs);
+    });
   }, []);
 
   useEffect(() => {
