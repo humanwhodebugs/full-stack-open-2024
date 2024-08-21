3c87296465020d4a28b8fb77cf54888ffd205444
Muhammad Rifai <muhmmd.rifaii@gmail.com>
2024-08-21 18:57:03 +0700

    5.23: Blog List End To End Testing, step 7

--- part5-e2e/tests/blog_app.spec.js
+++ part5-e2e/tests/blog_app.spec.js
@@ -40,15 +40,15 @@ describe('Blog app', () => {
   describe('Login', () => {
     test('succeeds with correct credentials', async ({ page }) => {
       await page.getByRole('button', { name: 'log in' }).click();
-      await page.fill('input[name="Username"]', 'testuser');
-      await page.fill('input[name="Password"]', 'testpassword');
+      await page.fill('input[name="Username"]', 'user1');
+      await page.fill('input[name="Password"]', 'password1');
       await page.getByRole('button', { name: 'Login' }).click();
-      await expect(page.getByText('Test User logged in')).toBeVisible();
+      await expect(page.getByText('User One logged in')).toBeVisible();
     });
 
     test('fails with wrong credentials', async ({ page }) => {
       await page.getByRole('button', { name: 'log in' }).click();
-      await page.fill('input[name="Username"]', 'testuser');
+      await page.fill('input[name="Username"]', 'user1');
       await page.fill('input[name="Password"]', 'wrongpassword');
       await page.getByRole('button', { name: 'Login' }).click();
       await expect(page.getByText('Wrong username or password!')).toBeVisible();
@@ -58,6 +58,10 @@ describe('Blog app', () => {
   describe('When logged in', () => {
     beforeEach(async ({ page }) => {
       await page.goto('http://localhost:5173');
+      await page.getByRole('button', { name: 'log in' }).click();
+      await page.fill('input[name="Username"]', 'user1');
+      await page.fill('input[name="Password"]', 'password1');
+      await page.getByRole('button', { name: 'Login' }).click();
     });
 
     test('a new blog can be created', async ({ page }) => {
@@ -69,54 +73,103 @@ describe('Blog app', () => {
 
       await expect(page.getByText('My New Blog John Doe')).toBeVisible();
     });
-  });
 
-  test('a blog can be liked', async ({ page }) => {
-    await page.getByText('Blog to be liked Jane Doe').click();
-
-    const likeButton = page.getByRole('button', { name: 'Like' });
-    const likesCount = page.locator('.likes');
-
-    const initialLikes = await likesCount.innerText();
-
-    await likeButton.click();
-
-    await expect(likesCount).toHaveText(`${parseInt(initialLikes) + 1}`);
-  });
-
-  test('the user who added the blog can delete it', async ({ page }) => {
-    await page.getByText('Blog to be deleted John Doe').click();
-
-    page.on('dialog', async (dialog) => {
-      expect(dialog.type()).toBe('confirm');
-      await dialog.accept();
-    });
-
-    await page.getByRole('button', { name: 'Remove' }).click();
-
-    await expect(
-      page.getByText('Blog to be deleted John Doe')
-    ).not.toBeVisible();
-  });
-
-  describe('When logged in as a different user', () => {
-    beforeEach(async ({ page }) => {
-      await page.goto('http://localhost:5173');
-
-      await page.getByRole('button', { name: 'log in' }).click();
-      await page.fill('input[name="Username"]', 'user2');
-      await page.fill('input[name="Password"]', 'password2');
-      await page.getByRole('button', { name: 'Login' }).click();
-    });
-
-    test('the delete button is not visible to users who did not create the blog', async ({
-      page,
-    }) => {
+    test('a blog can be liked', async ({ page }) => {
       await page.getByText('Blog created by user1 User One').click();
 
+      const likeButton = page.getByRole('button', { name: 'Like' });
+      const likesCount = page.locator('.likes');
+
+      const initialLikes = await likesCount.innerText();
+
+      await likeButton.click();
+
+      await expect(likesCount).toHaveText(`${parseInt(initialLikes) + 1}`);
+    });
+
+    test('the user who added the blog can delete it', async ({ page }) => {
+      await page.getByText('Blog created by user1 User One').click();
+
+      page.on('dialog', async (dialog) => {
+        expect(dialog.type()).toBe('confirm');
+        await dialog.accept();
+      });
+
+      await page.getByRole('button', { name: 'Remove' }).click();
+
       await expect(
-        page.getByRole('button', { name: 'Remove' })
+        page.getByText('Blog created by user1 User One')
       ).not.toBeVisible();
     });
+
+    describe('When logged in as a different user', () => {
+      beforeEach(async ({ page }) => {
+        await page.goto('http://localhost:5173');
+
+        await page.getByRole('button', { name: 'log in' }).click();
+        await page.fill('input[name="Username"]', 'user2');
+        await page.fill('input[name="Password"]', 'password2');
+        await page.getByRole('button', { name: 'Login' }).click();
+      });
+
+      test('the delete button is not visible to users who did not create the blog', async ({
+        page,
+      }) => {
+        await page.getByText('Blog created by user1 User One').click();
+
+        await expect(
+          page.getByRole('button', { name: 'Remove' })
+        ).not.toBeVisible();
+      });
+    });
+  });
+
+  test('blogs are ordered according to likes', async ({ page }) => {
+    await page.goto('http://localhost:5173');
+
+    const blogs = [
+      {
+        title: 'First Blog',
+        author: 'Author A',
+        url: 'http://example.com/a',
+        likes: 1,
+      },
+      {
+        title: 'Second Blog',
+        author: 'Author B',
+        url: 'http://example.com/b',
+        likes: 3,
+      },
+      {
+        title: 'Third Blog',
+        author: 'Author C',
+        url: 'http://example.com/c',
+        likes: 2,
+      },
+    ];
+
+    for (const blog of blogs) {
+      await page.getByRole('button', { name: 'Create Blog' }).click();
+      await page.fill('input[name="Title"]', blog.title);
+      await page.fill('input[name="Author"]', blog.author);
+      await page.fill('input[name="Url"]', blog.url);
+      await page.getByRole('button', { name: 'Create' }).click();
+
+      for (let i = 0; i < blog.likes; i++) {
+        await page
+          .getByText(blog.title)
+          .getByRole('button', { name: 'Like' })
+          .click();
+      }
+    }
+
+    const blogTitles = await page.$$eval('.blog-title', (elements) =>
+      elements.map((el) => el.innerText)
+    );
+    expect(blogTitles).toEqual([
+      'Second Blog Author B',
+      'Third Blog Author C',
+      'First Blog Author A',
+    ]);
   });
 });
